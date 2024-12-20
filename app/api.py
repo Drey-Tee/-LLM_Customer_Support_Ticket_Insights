0a529b7d9ac188db78bb392890be8805b52f034f
Drey-Tee <audreytetteh7@gmail.com>
2024-12-20 12:35:45 +0000

    port_fixes

--- app/api.py
+++ app/api.py
@@ -40,4 +40,5 @@ def predict():
 
 
 if __name__ == '__main__':
-    app.run(port=5001, debug=True)
+    port = int(os.environ.get("PORT", 5000))  # Default to 5000 if PORT is not set
+    app.run(host="0.0.0.0", port=port)
